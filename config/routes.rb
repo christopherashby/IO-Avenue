c8a2aee2cc370d19bacc9cff24ad58e2895c1d0d
Christopher Ashby <christopher.lee.ashby@gmail.com>
2018-08-08 23:36:54 +0000

    Blog_App

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  
+  get 'comments/index'
+
+    root to: 'posts#index'
+    resources :posts do
+        resources :comments
+    end
+        
+      # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
